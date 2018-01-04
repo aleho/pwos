5b3383ee43d621c726638c7783e987cd8e520614
Alexander Hofbauer <alex@derhofbauer.at>
2018-01-04 16:49:26 +0100

    Make sure vars file is commited on release

--- src/js/vars.js
+++ src/js/vars.js
@@ -1,5 +1,5 @@
-var PWOS_VERSION = 1515020997;
-var PWOS_DATE = '20180104_000957';
+var PWOS_VERSION = 1515079502;
+var PWOS_DATE = '20180104_162502';
 
 if (typeof window === 'undefined') {
     exports.PWOS_VERSION = PWOS_VERSION;
