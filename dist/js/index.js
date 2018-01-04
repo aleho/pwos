ad634ae209460d396d0c743aa61bc5e436e6b5d1
Alexander Hofbauer <alex@derhofbauer.at>
2018-01-04 16:50:16 +0100

    Release v20180104_165014

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,5 @@
-var PWOS_VERSION = 1515079502;
-var PWOS_DATE = '20180104_162502';
+var PWOS_VERSION = 1515081014;
+var PWOS_DATE = '20180104_165014';
 
 if (typeof window === 'undefined') {
     exports.PWOS_VERSION = PWOS_VERSION;
