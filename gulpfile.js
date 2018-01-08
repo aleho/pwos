7ff03ccdd2968354fd2a15f47dde7c56c88d209a
Alexander Hofbauer <alex@derhofbauer.at>
2018-01-08 23:26:54 +0100

    Upgrade to Gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ const DIST    = 'dist/';
 const JS_OUT  = DIST + 'js/';
 const CSS_OUT = DIST + 'css/';
 const TASKS   = [];
+const WATCHES = {};
 
 const JS_FILES = {
     'index': [
@@ -60,91 +61,102 @@ function addTasks(type, files, build, minify)
     let doMinify = (typeof minify == 'function');
 
     for (let group in files) {
-        let filesGroup   = files[group];
-        let taskname     = type + ': ' + group;
-        let filename     = group + '.' + type;
-        let tasknameMin  = doMinify ? taskname + '_minify' : taskname;
+        let filesGroup    = files[group];
+        let filename      = group + '.' + type;
+        let filenameMin   = group + '.min.' + type;
+        let taskname      = type + ': ' + group;
+        let tasknameBuild = taskname + ' [build]';
+        let tasknameMin   = taskname + ' [minify]';
+        let tasks         = [tasknameBuild];
 
-        gulp.task(taskname, build(filename, filesGroup, group));
+        gulp.task(tasknameBuild, function () {
+            return build(filename, filesGroup, group);
+        });
 
         if (doMinify) {
-            let filenameMin = group + '.min.' + type;
-            gulp.task(tasknameMin, [ taskname ], minify(filename, filenameMin, group));
+            tasks.push(tasknameMin);
+            gulp.task(tasknameMin, function () {
+                return minify(filename, filenameMin, group);
+            });
         }
+
+        gulp.task(taskname, gulp.series(tasks));
+        TASKS.push(taskname);
 
         if (OPTIONS.watch) {
-            gulp.watch(filesGroup, [ tasknameMin ]);
+            WATCHES[taskname] = filesGroup;
         }
-
-        TASKS.push(tasknameMin);
     }
 }
 
 
 addTasks('js', JS_FILES,
     function (filename, files) {
-        return function () {
-            return gulp
-                .on('error', function (err) { log('Err concat', err.toString()); })
-                .src(files)
-                .pipe(sourcemaps.init())
-                .pipe(concat(filename))
-                .pipe(sourcemaps.write('./'))
-                .pipe(gulp.dest(JS_OUT));
-        };
+        return gulp
+            .on('error', function (err) { log('Err concat', err.toString()); })
+            .src(files)
+            .pipe(sourcemaps.init())
+            .pipe(concat(filename))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(JS_OUT));
     },
     function (filename, filenameMin) {
-        return function () {
-            return gulp
-                .on('error', function (err) { log('Err min', err.toString()); })
-                .src([ JS_OUT + filename ])
-                .pipe(uglifyEs())
-                .pipe(concat(filenameMin))
-                .pipe(gulp.dest(JS_OUT));
-        };
+        return gulp
+            .on('error', function (err) { log('Err min', err.toString()); })
+            .src([ JS_OUT + filename ])
+            .pipe(uglifyEs())
+            .pipe(concat(filenameMin))
+            .pipe(gulp.dest(JS_OUT));
     }
 );
 
 addTasks('css', CSS_FILES,
     function (filename, files) {
-        return function () {
-            return gulp
-                .on('error', function (err) { log('Err concat', err.toString()); })
-                .src(files)
-                .pipe(sourcemaps.init())
-                .pipe(gulpIf(/[.]sass/, sass()))
-                .pipe(concat(filename))
-                .pipe(sourcemaps.write('./'))
-                .pipe(gulp.dest(CSS_OUT));
-        };
+        return gulp
+            .on('error', function (err) { log('Err concat', err.toString()); })
+            .src(files)
+            .pipe(sourcemaps.init())
+            .pipe(gulpIf(/[.]sass/, sass()))
+            .pipe(concat(filename))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(CSS_OUT));
     },
     function (filename, filenameMin) {
-        return function () {
-            return gulp
-                .on('error', function (err) { log('Err min', err.toString()); })
-                .src([ CSS_OUT + filename ])
-                .pipe(uglifyCss())
-                .pipe(concat(filenameMin))
-                .pipe(gulp.dest(CSS_OUT));
-        };
+        return gulp
+            .on('error', function (err) { log('Err min', err.toString()); })
+            .src([ CSS_OUT + filename ])
+            .pipe(uglifyCss())
+            .pipe(concat(filenameMin))
+            .pipe(gulp.dest(CSS_OUT));
     }
 );
 
 addTasks('html', HTML_FILES,
     function (filename, files) {
-        return function () {
-            return gulp
-                .on('error', function (err) { log(err.toString()); })
-                .src(files)
-                .pipe(replace(/"(js|css)\/([^"/.]+)\.(js|css)"/g, '"$1/$2.min.$3?v=' + vars.PWOS_DATE + '"'))
-                .pipe(gulp.dest(DIST));
-        };
+        return gulp
+            .on('error', function (err) { log(err.toString()); })
+            .src(files)
+            .pipe(replace(/"(js|css)\/([^"/.]+)\.(js|css)"/g, '"$1/$2.min.$3?v=' + vars.PWOS_DATE + '"'))
+            .pipe(gulp.dest(DIST));
     }
 );
 
 
-gulp.task('default', TASKS, function () {
-    if (OPTIONS.watch) {
+let gulpSeries = ['build'];
+gulp.task('build', gulp.parallel(TASKS));
+
+if (OPTIONS.watch) {
+    gulpSeries.push('watch');
+    gulp.task('watch', function (done) {
         console.log('\u001b[33m' + 'Now watching for file changes...' + '\u001b[39m');
-    }
-});
+
+        for (let task in WATCHES) {
+            let files = WATCHES[task];
+            gulp.watch(files, gulp.series(task));
+        }
+
+        done();
+    });
+}
+
+gulp.task('default', gulp.series(gulpSeries));
