3cd264140aa6cf7ca26923636c6dae10f045f32f
Alexander Hofbauer <alex@derhofbauer.at>
2018-01-28 22:31:07 +0100

    Release v20180128_223100

--- src/js/vars.js
+++ src/js/vars.js
@@ -1,5 +1,5 @@
-var PWOS_VERSION = 1515523392;
-var PWOS_DATE = '20180109_194312';
+var PWOS_VERSION = 1517175060;
+var PWOS_DATE = '20180128_223100';
 
 if (typeof window === 'undefined') {
     exports.PWOS_VERSION = PWOS_VERSION;
